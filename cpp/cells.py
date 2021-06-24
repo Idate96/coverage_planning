9f08af835db8bdc551b6bbf4c3f0d66288f8e8bb
Lorenzo <lorenzoterenzi96@gmail.com>
2021-06-24 12:08:57 +0200

    added tests for producing cells from decomposed images

--- cpp/cells.py
+++ cpp/cells.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import List
 
 class Cell(object): 
-    def __init__(self, x_left:int=0, x_right:int=0) -> None:
+    def __init__(self, x_left:int=0, x_right:int=0, cell_id:int=0) -> None:
         super().__init__()
         # mapping x->y, O(1) contains operation
         self.top = {}
@@ -12,15 +12,16 @@ class Cell(object):
         self.x_right = x_right
         self.left = []
         self.right = []
+        self.cell_id = cell_id
 
     @classmethod
-    def from_image(decomposed_image: np.array):
-        num_cells = np.max(decomposed_image)
-        width, height = np.shape(decomposed_image)
-        cells =  [Cell(0, width) for i in range(num_cells)]
+    def from_image(cls, decomposed_image: np.array):
+        num_cells = len(set(decomposed_image.flatten()))
+        height, width = np.shape(decomposed_image)
+        cells =  [Cell(0, width - 1, i) for i in range(num_cells)]
         for x in range(width):
             for y in range(height):
-                cell = cells[decomposed_image[x, y]]
+                cell = cells[decomposed_image[y, x]]
                 if x < cell.x_left: 
                     cell.x_left = x
                     cell.left = [y]
@@ -35,9 +36,17 @@ class Cell(object):
 
                 if (x not in cell.bottom):
                     cell.bottom[x] = y
-                
+                # is there a better way to avoid the key error          
                 elif cell.bottom[x] > y:
-                    cell.bottom[y]
+                    cell.bottom
+
+                if (x not in cell.top):
+                    cell.top[x] = y 
+                elif cell.top[x] < y:
+                    cell.top[x] = y
+        return cells
+
+
 
 
                 
---
+++ tests/play.py
@@ -0,0 +1,6 @@
+from cpp.cells import Cell
+
+
+cells = [Cell(0, 10, i) for i in range(10)]
+
+cell = filter()
