089daef7a1bc213095beeac622211604c9e69aa5
Lorenzo <lorenzoterenzi96@gmail.com>
2021-07-19 17:42:46 +0200

    tested mst for concave objects

--- tests/test_dfs_tree.py
+++ tests/test_dfs_tree.py
@@ -2,10 +2,13 @@ from collections import defaultdict
 
 from numpy.lib.shape_base import expand_dims
 from cpp.dfs_tree import Graph, adj_matrix_to_dict
-from cpp.bsd import create_global_adj_matrix
+from cpp.bsd import *
 import numpy as np
 import matplotlib.image as mpimg
+from cpp.dfs_tree import *
 from typing import Tuple, List
+from cpp.helpers import *
+from cpp.cells import *
 
 
 def create_graph():
@@ -126,15 +129,14 @@ def test_create_graph_from_image():
     image = mpimg.imread("data/test/map.jpg")
     # original image is black and white anyway
     binary_image = image[:, :, 0] > 150
-    graph_adj_matrix = create_global_adj_matrix(binary_image)
+    graph_adj_matrix, _ = create_global_adj_matrix(binary_image)
     graph_adj_dict = adj_matrix_to_dict(graph_adj_matrix)
-    
     graph = Graph(graph_adj_dict)
     tree_edges = graph.DFS_tree(0)
     graph_tree = Graph()
     graph_tree.add_edges(tree_edges, directed=False)
     diameter, new_root, furthest_node = graph_tree.find_diameter(0)
-    
+
     tree_edges = graph_tree.DFS_tree(new_root)
     longest_tree = Graph()
     longest_tree.add_edges(tree_edges)
@@ -143,11 +145,127 @@ def test_create_graph_from_image():
     assert visited == expected_visit
 
 
+def test_create_arcs_from_image():
+    binary_image = recurvise_H_map((400, 600), invert=True, num_recursions=0)
+    graph_adj_matrix, decomposed_im = get_directed_global_adj_matrix(binary_image)
+    graph_adj_dict = adj_matrix_to_dict(graph_adj_matrix)
+    graph = Graph(graph_adj_dict)
+    arc_edges = graph.graph_to_arcs()
+    list_expected_arcs = [
+        Arc(tail=0, weight=1, head=1),
+        Arc(tail=0, weight=1, head=3),
+        Arc(tail=1, weight=1, head=0),
+        Arc(tail=1, weight=1, head=5),
+        Arc(tail=2, weight=1, head=0),
+        Arc(tail=3, weight=1, head=0),
+        Arc(tail=3, weight=1, head=5),
+        Arc(tail=4, weight=1, head=5),
+        Arc(tail=5, weight=1, head=1),
+        Arc(tail=5, weight=1, head=3),
+    ]
+    for arc in arc_edges:
+        assert arc in list_expected_arcs
+
+
+def test_create_arcs_dfs():
+    binary_image = recurvise_H_map((400, 600), invert=True, num_recursions=0)
+    graph_adj_matrix, decomposed_im = get_directed_global_adj_matrix(binary_image)
+    graph_adj_dict = adj_matrix_to_dict(graph_adj_matrix)
+    graph = Graph(graph_adj_dict)
+    arc_edges = graph.graph_to_arcs()
+    arc_edges_dsf = graph.graph_to_arcs_with_dfs(0)
+
+
+    for arc in arc_edges:
+        assert arc in arc_edges_dsf
+    for arc in arc_edges_dsf:
+        assert arc in arc_edges
+
+
+def test_create_graph_from_edges():
+    binary_image = recurvise_H_map((400, 600), invert=True, num_recursions=0)
+    graph_adj_matrix, decomposed_im = get_directed_global_adj_matrix(binary_image)
+    graph_adj_dict = adj_matrix_to_dict(graph_adj_matrix)
+    graph = Graph(graph_adj_dict)
+
+    arc_edges = graph.graph_to_arcs()
+    graph_from_arcs = Graph()
+    graph_from_arcs.arcs_to_graph(arc_edges)
+
+    assert graph_from_arcs.adj == graph.adj
+
+
 def add_num_to_list(list: List[int], num: int):  # O(n)
     for i in range(len(list)):
         list[i] += num
     return list
 
 
+def test_minimum_spanning_tree_directed_graph():
+    binary_image = recurvise_H_map((400, 600), invert=True, num_recursions=0)
+    graph_adj_matrix, decomposed_im = get_directed_global_adj_matrix(binary_image)
+    graph_adj_dict = adj_matrix_to_dict(graph_adj_matrix)
+    graph = Graph(graph_adj_dict)
+    arc_edges = graph.graph_to_arcs()
+    arc_edges_dfs = [
+        Arc(tail=0, weight=1, head=1),
+        Arc(tail=1, weight=1, head=5),
+        Arc(tail=5, weight=1, head=3),
+        Arc(tail=3, weight=1, head=0),
+        Arc(tail=0, weight=1, head=3),
+        Arc(tail=3, weight=1, head=5),
+        Arc(tail=5, weight=1, head=1),
+        Arc(tail=1, weight=1, head=0),
+        Arc(tail=2, weight=1, head=0),
+        Arc(tail=4, weight=1, head=5),
+    ]
+
+    print(arc_edges)
+    mst = min_spanning_arborescence(arc_edges, 0)
+    mst_dfs = min_spanning_arborescence(arc_edges_dfs, 0)
+    print(mst)
+    print(mst_dfs)
+
+def test_mst_dfs_directed_graph():
+    binary_image = recurvise_H_map((400, 600), invert=True, num_recursions=0)
+    graph_adj_matrix, decomposed_im = get_directed_global_adj_matrix(binary_image)
+    graph_adj_dict = adj_matrix_to_dict(graph_adj_matrix)
+    graph = Graph(graph_adj_dict)
+    start_node = 0
+    arc_edges = graph.graph_to_arcs_with_dfs(start_node)
+    mst = min_spanning_arborescence(arc_edges, start_node)
+    # dfs like tree 
+    tree = {2: Arc(tail=2, weight=1, head=0), 3: Arc(tail=3, weight=1, head=0), 5: Arc(tail=5, weight=1, head=3), 4: Arc(tail=4, weight=1, head=5), 1: Arc(tail=1, weight=1, head=5)}
+    assert mst == tree
+
+def test_reverse_graph():
+    binary_image = recurvise_H_map((400, 600), invert=True, num_recursions=0)
+    graph_adj_matrix, decomposed_im = get_directed_global_adj_matrix(binary_image)
+    graph_adj_dict = adj_matrix_to_dict(graph_adj_matrix)
+    graph = Graph(graph_adj_dict)
+    start_node = 0
+    arc_edges = graph.graph_to_arcs_with_dfs(start_node)
+    mst_arcs = min_spanning_arborescence(arc_edges, start_node).values()
+    mst = Graph()
+    mst.arcs_to_graph(mst_arcs)
+    mst.reverse_graph_edges()
+    expected_dict = {2: [], 0: [2, 3], 3: [5], 5: [4, 1], 4: [], 1: []}
+    assert mst.adj == expected_dict
+
+def test_post_order_traversal_mst():
+    binary_image = recurvise_H_map((400, 600), invert=True, num_recursions=0)
+    graph_adj_matrix, decomposed_im = get_directed_global_adj_matrix(binary_image)
+    graph_adj_dict = adj_matrix_to_dict(graph_adj_matrix)
+    graph = Graph(graph_adj_dict)
+    start_node = 0
+    arc_edges = graph.graph_to_arcs_with_dfs(start_node)
+    mst_arcs = min_spanning_arborescence(arc_edges, start_node).values()
+    mst = Graph()
+    mst.arcs_to_graph(mst_arcs)
+    mst.reverse_graph_edges()
+    post_order_traversal = mst.post_order_traversal(start_node)
+    expected_post_order_traversal = [2, 4, 1, 5, 3, 0]
+    assert post_order_traversal == expected_post_order_traversal
+
 if __name__ == "__main__":
-    test_create_graph_from_image()
+    test_post_order_traversal_mst()
